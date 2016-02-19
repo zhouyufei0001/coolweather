a95cab8f175854c1ebca608e26ae073c5feb1296
zhouyufei <724256411@qq.com>
2016-02-19 21:31:17 +0800

    新增切换城市和手动更新天气的功能

--- src/activity/ChooseAreaActivity.java
+++ src/activity/ChooseAreaActivity.java
@@ -32,6 +32,11 @@ import android.widget.Toast;
 
 public class ChooseAreaActivity extends Activity {
 	
+	/**
+	 * 是否从WeatherActivity中跳转过来。
+	 */
+	private boolean isFromWeatherActivity;
+
 
 		public static final int LEVEL_PROVINCE = 0;
 		public static final int LEVEL_CITY = 1;
@@ -71,8 +76,9 @@ public class ChooseAreaActivity extends Activity {
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
+			isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_ activity", false);
 			SharedPreferences prefs = PreferenceManager. getDefaultSharedPreferences(this);
-			if (prefs.getBoolean("city_selected", false)) {
+			if (prefs.getBoolean("city_selected", false)&& !isFromWeatherActivity) {
 				Intent intent = new Intent(this, WeatherActivity.class);
 				startActivity(intent);
 				finish();
@@ -256,6 +262,11 @@ public class ChooseAreaActivity extends Activity {
 			} else if (currentLevel == LEVEL_CITY) {
 				queryProvinces();
 			} else {
+				if (isFromWeatherActivity) {
+					Intent intent = new Intent(this, WeatherActivity.class);
+					startActivity(intent);
+				}
+
 				finish();
 			}
 		}
--- src/activity/WeatherActivity.java
+++ src/activity/WeatherActivity.java
@@ -7,6 +7,7 @@ import util.Utility;
 import com.coolweather.app.R;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -19,7 +20,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity{
+public class WeatherActivity extends Activity implements OnClickListener{
+
 
 	private LinearLayout weatherInfoLayout;
 	/**
@@ -80,9 +82,34 @@ public class WeatherActivity extends Activity{
 			// 没有县级代号时就直接显示本地天气
 			showWeather();
 		}
+		switchCity = (Button) findViewById(R.id.switch_city);
+		refreshWeather = (Button) findViewById(R.id.refresh_weather);
+		switchCity.setOnClickListener((android.view.View.OnClickListener) this);
+		refreshWeather.setOnClickListener((android.view.View.OnClickListener) this);
+
 		
 	}
-	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.switch_city:
+			Intent intent = new Intent(this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresh_weather:
+			publishText.setText("同步中...");
+			SharedPreferences prefs = PreferenceManager. getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if (!TextUtils.isEmpty(weatherCode)) {
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
 	
 	
 	/**
@@ -154,6 +181,11 @@ public class WeatherActivity extends Activity{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+	}
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO 自动生成的方法存根
+		
 	}
 
 }
