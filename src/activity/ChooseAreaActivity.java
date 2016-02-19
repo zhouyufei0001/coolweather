47dba1b33125d52f23e4fa5efeb5148c82988784
zhouyufei <724256411@qq.com>
2016-02-19 21:09:30 +0800

    加入显示天气信息的功能.

--- src/activity/ChooseAreaActivity.java
+++ src/activity/ChooseAreaActivity.java
@@ -15,7 +15,10 @@ import model.County;
 import model.Province;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -68,6 +71,14 @@ public class ChooseAreaActivity extends Activity {
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
+			SharedPreferences prefs = PreferenceManager. getDefaultSharedPreferences(this);
+			if (prefs.getBoolean("city_selected", false)) {
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+				finish();
+				return;
+			}
+
 			requestWindowFeature(Window.FEATURE_NO_TITLE);
 			setContentView(R.layout.choose_area);
 			listView = (ListView) findViewById(R.id.list_view);
@@ -86,6 +97,14 @@ public class ChooseAreaActivity extends Activity {
 						selectedCity = cityList.get(index);
 						queryCounties();
 					}
+					else if (currentLevel == LEVEL_COUNTY) {
+						String countyCode = countyList.get(index).getCountyCode();
+						Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+						intent.putExtra("county_code", countyCode);
+						startActivity(intent);
+						finish();
+					}
+
 				}
 			});
 			queryProvinces();  // 加载省级数据
